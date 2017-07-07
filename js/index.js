345fb9715e3e561a8384c8e166ac9b61376057f3
lxuans <837865347@qq.com>
2017-07-07 09:42:18 +0800

    C添加了点击事件, 点击图片桌面换肤

---
+++ js/index.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+   
+        var lis = document.querySelectorAll('.pic>ul>li');
+        for (var i = 0; i < lis.length; i++) {
+            var menu = lis[i].children[0];
+            menu.src = "./images/" + (i+1) + ".jpg";
+
+            menu.onclick = function(){
+                document.body.style.backgroundImage = "url(" + this.src + ")";
+                document.querySelector('.box').style.backgroundColor = "rgba(255,255,255,.3)";
+            }
+        }
+   
+}
